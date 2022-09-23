677f682d3eab46ba998d060240372614069776dd
rodoggyro <Fiyrflash@gmail.com>
2022-09-23 17:22:06 -0400

    encoders working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoder_crane.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoder_crane.java
@@ -3,10 +3,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.sun.source.tree.WhileLoopTree;
 
 
 @Autonomous
-public class encoder_auto extends LinearOpMode {
+public class encoder_crane extends LinearOpMode {
 
 
     private DcMotor crane;
@@ -19,10 +20,19 @@ public class encoder_auto extends LinearOpMode {
         if (opModeIsActive()) {
 
             //crane(1,1000);
-            crane.setTargetPosition(100);
+            crane.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            crane.setTargetPosition(1000);
             crane.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             crane.setPower(1);
-            sleep(8000);
+            while (crane.isBusy()){
+
+            }
+            crane.setTargetPosition(500);
+            crane.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            crane.setPower(1);
+            while (crane.isBusy()){
+                
+            }
 
 
             telemetry.addData("encoder value", crane.getCurrentPosition());
