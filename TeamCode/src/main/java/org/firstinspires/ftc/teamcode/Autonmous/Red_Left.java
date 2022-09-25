cfaacf1ec7a4bcfb543bcab3481f517c24300a29
Fiyrflash <113477109+Fiyrflash@users.noreply.github.com>
2022-09-25 19:39:52 -0400

    Red Left test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous/Red_Left.java
@@ -0,0 +1,292 @@
+package org.firstinspires.ftc.teamcode.Autonmous;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.sun.source.tree.WhileLoopTree;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+//import org.openftc.easyopencv.OpenCvCamera;
+//import org.openftc.easyopencv.OpenCvCameraFactory;
+//import org.openftc.easyopencv.OpenCvCameraRotation;
+//import org.openftc.easyopencv.OpenCvWebcam;
+import static java.lang.Thread.sleep;
+
+
+@Autonomous
+public class Red_Left extends LinearOpMode {
+
+
+    private DcMotor crane;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    public int side = 2;
+    public Orientation angles;
+    public BNO055IMU imu;
+
+    public void runOpMode() {
+        crane = hardwareMap.get(DcMotor.class, "crane");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+
+
+            /*scan cone
+
+             */
+
+            /*if (side = 1){}
+            else if(side = 2){}
+            else if(side = 3){}
+            else {
+                telemetry.addLine("not detected");
+                telemetry.update();
+            }*/
+
+
+            //move to cones
+            move(1,1440);
+
+            //turn to face cones
+            gyroTurning(90);
+
+            //arm stuff
+
+            //turn to face stick
+            gyroTurning(180);
+
+            //arm stuff
+
+            //turn to face cones
+            gyroTurning(180);
+
+            //arm stuff
+
+            //turn to face stick
+            gyroTurning(180);
+
+            //arm stuff
+
+            //turn to face cones
+            gyroTurning(180);
+
+            //arm stuff
+            //turn to face stick
+            gyroTurning(180);
+
+            //arm stuff
+
+            //turn to face cones
+            gyroTurning(180);
+
+            //arm stuff
+            //turn to face stick
+            gyroTurning(180);
+
+            //arm stuff
+
+            //turn to face cones
+            gyroTurning(180);
+
+            //arm stuff
+            //turn to face stick
+            gyroTurning(180);
+
+            //arm stuff
+
+            ////////////////////////////////////////////////////////////////////////
+
+            StrafeRight(1,2000);
+
+
+            if (side ==1){
+                move(1, 720);
+
+            }
+            else if (side ==3){
+                move(1,-720);
+
+            }
+
+            else{
+
+            }
+
+
+
+
+        }
+    }
+
+    public boolean gyroTurning(double targetAngle) {
+        boolean foundAngle = false;
+        //while (foundAngle == false || did I reach time limit) {
+        while (foundAngle == false) {
+            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            double currentAngle = angles.firstAngle;
+
+            if (angles.firstAngle >= targetAngle - 0.25 && angles.firstAngle <= targetAngle + 0.25) {
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backLeft.setPower(0);
+                backRight.setPower(0);
+                foundAngle = true;
+                sleep(1000);
+                break;
+            } else if (angles.firstAngle >= targetAngle + 0.5) {
+                if (angles.firstAngle <= targetAngle + 10) {
+                    frontLeft.setPower(0.15);
+                    frontRight.setPower(-0.15);
+                    backLeft.setPower(0.15);
+                    backRight.setPower(-0.15);
+                    foundAngle = false;
+                } else {
+                    frontLeft.setPower(0.5);
+                    frontRight.setPower(-0.5);
+                    backLeft.setPower(0.5);
+                    backRight.setPower(-0.5);
+                    foundAngle = false;
+                }
+            } else if (angles.firstAngle <= targetAngle - 0.5) {
+                if (angles.firstAngle >= targetAngle - 10) {
+                    frontLeft.setPower(-0.15);
+                    frontRight.setPower(0.15);
+                    backLeft.setPower(-0.15);
+                    backRight.setPower(0.15);
+                    foundAngle = false;
+                } else {
+                    frontLeft.setPower(-0.5);
+                    frontRight.setPower(0.5);
+                    backLeft.setPower(-0.5);
+                    backRight.setPower(0.5);
+                    foundAngle = false;
+                }
+            }
+        }
+        return foundAngle;
+    }
+
+    public void move(double power, int position) {
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRight.setTargetPosition(-position);
+        frontLeft.setTargetPosition(-position);
+        backRight.setTargetPosition(-position);
+        backLeft.setTargetPosition(-position);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontRight.setPower(power);
+        frontLeft.setPower(power);
+        backRight.setPower(power);
+        backLeft.setPower(power);
+
+        while (frontLeft.isBusy()) {
+
+        }
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+        backRight.setPower(0);
+        backLeft.setPower(0);
+    }
+
+   public void StrafeRight(double power, int position){
+       frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+       backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+       frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+       backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+       frontRight.setTargetPosition(-position);
+       frontLeft.setTargetPosition(position);
+       backRight.setTargetPosition(position);
+       backLeft.setTargetPosition(-position);
+
+       frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+       frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+       backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+       backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+       frontRight.setPower(power);
+       frontLeft.setPower(power);
+       backRight.setPower(power);
+       backLeft.setPower(power);
+
+       while (frontLeft.isBusy()) {
+
+       }
+       frontRight.setPower(0);
+       frontLeft.setPower(0);
+       backRight.setPower(0);
+       backLeft.setPower(0);
+
+    }
+
+    public void StrafeLeft(double power, int position){
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRight.setTargetPosition(position);
+        frontLeft.setTargetPosition(-position);
+        backRight.setTargetPosition(-position);
+        backLeft.setTargetPosition(position);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontRight.setPower(power);
+        frontLeft.setPower(power);
+        backRight.setPower(power);
+        backLeft.setPower(power);
+
+        while (frontLeft.isBusy()) {
+
+        }
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+        backRight.setPower(0);
+        backLeft.setPower(0);
+    }
+
+
+
+
+
+
+}
+
+
+
