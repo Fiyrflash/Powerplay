0ab7517a5bc9d3dd8d29370e4dca198d2a87d808
Fiyrflash <Fiyrflash@gmail.com>
2022-11-16 19:45:54 -0500

    test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Detector.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.opencv.core.Rect;
+
+public class Detector extends OpenCvPipeline {
+    Telemetry telemetry;
+    Mat mat = new Mat();
+
+    public enum Location {
+        SIDE1,
+        SIDE2,
+        SIDE3,
+        NOT_FOUND
+    }
+    public Location location;
+
+
+    static final Rect MID_ROI = new Rect(
+            new Point(140, 10),
+            new Point(170, 150));
+
+
+    static double percentThreshold = 0.05;
+
+    public Detector(Telemetry t){
+        telemetry = t;
+    }
+
+    @Override
+    public Mat processFrame(Mat input){
+        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+
+        // Yellowish
+        Scalar yellowLowHSV = new Scalar(23,70,70);
+        Scalar yellowHighHSV = new Scalar(32, 255, 255);
+
+        // Orangish
+        Scalar orangeLowHSV = new Scalar(0, 59, 107);
+        Scalar orangeHighHSV = new Scalar(71, 183, 255);
+
+        //green
+        Scalar greenLowHSV = new Scalar(40,40,40);
+        Scalar greenHighHSV = new Scalar(70,255,255);
+
+        Core.inRange(mat, greenLowHSV, greenHighHSV, mat);
+
+
+        Mat mid = mat.submat(MID_ROI);
+
+
+
+        double midValue = Core.sumElems(mid).val[0] / MID_ROI.area() / 100;
+
+
+        mid.release();
+
+        telemetry.addData("raw Mid value", (int) Core.sumElems(mid).val[0]);
+        telemetry.addData("Mid percentage", Math.round(midValue * 100) + "%");
+
+
+        boolean tseMid = midValue > percentThreshold;
+
+
+
+        if (tseMid){
+            location = Location.SIDE3;
+            telemetry.addData("Location", "Middle");
+
+        }
+        telemetry.update();
+
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2RGB);
+
+        Scalar tseColor = new Scalar(255,0,0);
+
+
+        Imgproc.rectangle(mat, MID_ROI, tseColor);
+
+        return mat;
+    }
+    public Location getLocation() {
+        return location;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueLeft.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+public class blueLeft extends LinearOpMode{
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    OpenCvWebcam webcam;
+
+    private CRServo Right;
+    private CRServo Left;
+    private DcMotor Crane;
+    private DcMotor Spin;
+
+
+    public void runOpMode() throws InterruptedException {
+
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        Right = hardwareMap.get(CRServo.class, "Rights");
+        Left = hardwareMap.get(CRServo.class, "Lefts");
+        Crane = hardwareMap.get(DcMotor.class, "Crane");
+        Spin = hardwareMap.get(DcMotor.class, "Spin");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+
+        int cameraMonitorViewId = hardwareMap.appContext
+                .getResources().getIdentifier("cameraMonitorViewId",
+                        "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        side1 detector = new side1(telemetry);
+        side2 j = new side2(telemetry);
+        Detector d =new Detector(telemetry);
+
+        webcam.setPipeline(detector);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+
+        waitForStart();
+        if (opModeIsActive()){
+            switch (detector.getLocation()) {
+                case SIDE1:
+                    break;
+            }
+
+            switch (d.getLocation()) {
+                case SIDE3:
+                    break;
+            }
+            switch (j.getLocation()) {
+                case SIDE2:
+                    break;
+            }
+
+
+
+        }
+    }
+}
