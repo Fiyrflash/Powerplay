30982ec0b08328aedd7d7fbd09cefa9de91ae546
rodoggyro <Fiyrflash@gmail.com>
2023-01-07 12:53:26 -0500

    starting work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueLeft.java
@@ -1,4 +1,6 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -6,6 +8,11 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.openftc.apriltag.AprilTagDetection;
+@Autonomous
 public class blueLeft extends LinearOpMode {
     private DcMotor frontLeft;
     private DcMotor frontRight;
@@ -15,7 +22,7 @@ public class blueLeft extends LinearOpMode {
 
     //private CRServo Right;
     private CRServo Left;
-    private DcMotor Crain;
+    private DcMotor Crane;
     private DcMotor Spin;
 
 
@@ -29,7 +36,7 @@ public class blueLeft extends LinearOpMode {
 
         //Right = hardwareMap.get(CRServo.class,"Rights");
         Left = hardwareMap.get(CRServo.class, "Lefts");
-        Crain = hardwareMap.get(DcMotor.class, "Crane");
+        Crane = hardwareMap.get(DcMotor.class, "Crane");
         //Spin = hardwareMap.get(DcMotor.class, "Spin");
 
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -41,8 +48,219 @@ public class blueLeft extends LinearOpMode {
 
         if (opModeIsActive()) {
 
+        move(1,500);
+
         }
 
+
+    }
+
+    public void stopMotors() {
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+
+    public void move(double power, int position) {
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRight.setTargetPosition(-position);
+        frontLeft.setTargetPosition(-position);
+        backRight.setTargetPosition(-position);
+        backLeft.setTargetPosition(-position);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontRight.setPower(power);
+        frontLeft.setPower(power);
+        backRight.setPower(power);
+        backLeft.setPower(power);
+
+        while (frontLeft.isBusy() && opModeIsActive()) {
+
+        }
+
+    }
+    public void moveandspin(double power, int position,double powers, int positions) {
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRight.setTargetPosition(-position);
+        frontLeft.setTargetPosition(-position);
+        backRight.setTargetPosition(-position);
+        backLeft.setTargetPosition(-position);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontRight.setPower(power);
+        frontLeft.setPower(power);
+        backRight.setPower(power);
+        backLeft.setPower(power);
+        spin2(powers,positions);
+        while (frontLeft.isBusy() && opModeIsActive()) {
+
+        }
+    }
+    public void craneinput(int time) {
+        crane(1,time);
+        intake(1,1000);
+        sleep(1500);
+    }
+
+    public void strafeLeftwithcrane(double power, int position,double powerc, int timec)  {
+
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRight.setTargetPosition(-position);
+        frontLeft.setTargetPosition(position);
+        backRight.setTargetPosition(position);
+        backLeft.setTargetPosition(-position);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontRight.setPower(power);
+        frontLeft.setPower(power);
+        backRight.setPower(power);
+        backLeft.setPower(power);
+        crane(powerc,timec);
+        while (frontLeft.isBusy() && opModeIsActive()) {
+        }
+
+    }
+    public void strafeRightwithcrane(double power, int position,double powerc, int timec)  {
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRight.setTargetPosition(position);
+        frontLeft.setTargetPosition(-position);
+        backRight.setTargetPosition(-position);
+        backLeft.setTargetPosition(position);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontRight.setPower(power);
+        frontLeft.setPower(power);
+        backRight.setPower(power);
+        backLeft.setPower(power);
+        crane(powerc,timec);
+        while (frontLeft.isBusy() && opModeIsActive()) {
+
+        }
+    }
+    public void strafeLeft(double power, int position)  {
+
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRight.setTargetPosition(-position);
+        frontLeft.setTargetPosition(position);
+        backRight.setTargetPosition(position);
+        backLeft.setTargetPosition(-position);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontRight.setPower(power);
+        frontLeft.setPower(power);
+        backRight.setPower(power);
+        backLeft.setPower(power);
+        crane(-1,2100);
+        while (frontLeft.isBusy() && opModeIsActive()) {
+        }
+
+    }
+    public void strafeRight(double power, int position)  {
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRight.setTargetPosition(position);
+        frontLeft.setTargetPosition(-position);
+        backRight.setTargetPosition(-position);
+        backLeft.setTargetPosition(position);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontRight.setPower(power);
+        frontLeft.setPower(power);
+        backRight.setPower(power);
+        backLeft.setPower(power);
+
+        while (frontLeft.isBusy() && opModeIsActive()) {
+
+        }
+    }
+
+    public void intake(int direction, long time)  {
+        Left.setPower(direction );
+        sleep(time);
+        Left.setPower(0);
+
+    }
+
+
+    public void spin(double power, int position) {
+        telemetry.addData("spin", Spin.getCurrentPosition());
+        telemetry.update();
+        Spin.setTargetPosition(position);
+        Spin.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Spin.setPower(power);
+        while (Spin.isBusy()&&opModeIsActive()) {
+
+        }
+    }
+
+    public void crane(double power, int time) {
+        Crane.setPower(power);
+        sleep(time);
+        Crane.setPower(0);
+
+    }
+    public void crane2(double power, int position) {
+        Crane.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Crane.setTargetPosition(position);
+        Crane.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Crane.setPower(power);
+        while (Crane.isBusy()&&opModeIsActive()) {
+
+        }
+        Crane.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+    public void spin2(double power, int time) {
+        Spin.setPower(power);
+        sleep(time);
+        Spin.setPower(0);
 
     }
 }
