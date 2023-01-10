8f4404c76fd3bc03a8c16a9bf7dd639e86d98869
KingTryHard1 <codeshb7@gmail.com>
2023-01-10 15:58:53 -0500

    Cleaned up more stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueLeft.java
@@ -142,8 +142,6 @@ public class blueLeft extends LinearOpMode {
         Crane.setPower(0);
     }
 
-
-
         public void intake(double power, int time){
             intake.setPower(power);
             sleep(time);
