75446a72c62f95afb3a21bbd6ce072dce0289a29
KingTryHard1 <codeshb7@gmail.com>
2023-01-23 11:28:27 -0500

    craneTest is added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/craneTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/craneTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -14,22 +15,38 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@teleop
-public class blueLeft extends LinearOpMode {
+@TeleOp
+public class craneTest extends LinearOpMode {
 
     private DcMotor Crane;
 
     public void runOpMode() throws InterruptedException {
-    
-      Crane = hardwareMap.get(DcMotor.class, "Crane");
-    
-      Crane.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-      
-      waitForStart();
-      if (opModeIsActive)) {
-      
-      
-      
-      }
 
-      
+        Crane = hardwareMap.get(DcMotor.class, "Crane");
+
+        Crane.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+        while (opModeIsActive()) {
+
+            double cranepower;
+
+            cranepower = gamepad2.right_stick_y;
+
+            Crane.setPower(cranepower);
+
+            telemetry.addData("crane",Crane.getCurrentPosition());
+            telemetry.update();
+
+            if (gamepad1.a) {
+
+
+
+
+
+            }
+        }
+    }
+}
+
+
