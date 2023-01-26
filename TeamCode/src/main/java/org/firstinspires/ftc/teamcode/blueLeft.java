22893ede0ed8341a904694b765c82a1537d3d66b
KingTryHard1 <codeshb7@gmail.com>
2023-01-26 12:46:50 -0500

    Camera is the new era

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueLeft.java
@@ -14,7 +14,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-import ConeCam.AprilTagAutonomousinitDetectionExample;
+import ConeCam.AprilTagAutonomousInitDetectionExample;
 
 @Autonomous
 public class blueLeft extends LinearOpMode {
