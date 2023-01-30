fbefbccd674ebb1595f49fe4649d37f2ab5f456b
KingTryHard1 <codeshb7@gmail.com>
2023-01-30 11:15:56 -0500

    oneplayer.java now works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/oneplayer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/oneplayer.java
@@ -1,12 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Disabled
+@TeleOp
 public class oneplayer extends LinearOpMode {
 
     private DcMotor frontLeft;
