63e45be00b2e0220d3b181188b389a99e445ad55
KingTryHard1 <codeshb7@gmail.com>
2023-03-30 17:57:51 -0400

    FSM .1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FiniteStateMachine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FiniteStateMachine.java
@@ -2,105 +2,104 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class FiniteStateMachine {
-    @TeleOp (group = "FSM")
-    public class FSMExample extends driveConstantsFSM {
-
+    @TeleOp(name = "FSM Example")
+    public class FSMExample extends OpMode {
         // An Enum is used to represent lift states.
         // (This is one thing enums are designed to do)
         public enum CraneState {
             CRANE_START,
             CRANE_UP,
-            CRANE_DOWN,
-            CRANE_DROP
-        };
+            CRANE_DOWN
+        }
 
+        ;
+
+        // The liftState variable is declared out here
+        // so its value persists between loop() calls
         CraneState CS = CraneState.CRANE_START;
 
+        // Some hardware access boilerplate; these would be initialized in init()
+        // the lift motor, it's in RUN_TO_POSITION mode
+        public DcMotorEx crane;
+
         // the dump servo
+        public Servo lefts;
         // used with the dump servo, this will get covered in a bit
-        ElapsedTime cranetimer = new ElapsedTime();
 
-        final double DROP_IDLE; // the idle position for the dump servo
-        final double DROP_DEPOSIT; // the dumping position for the dump servo
-
-        // the amount of time the dump servo takes to activate in seconds
-        final double DUMP_TIME;
-
-        final int LIFT_LOW; // the low encoder position for the lift
-        final int LIFT_HIGH; // the high encoder position for the lift
+        final int CRANE_LOW; // the low encoder position for the lift
+        final int CRANE_MID;
+        final int CRANE_HIGH; // the high encoder position for the lift
 
         public void init() {
-            cranetimer.reset();
+            liftTimer.reset();
 
-            crane.setTargetPosition(LIFT_LOW);
+            // hardware initialization code goes here
+            // this needs to correspond with the configuration used
+            crane = hardwareMap.get(DcMotorEx.class, "CraneFront");
+            lefts = hardwareMap.get(Servo.class, "LeftsFront");
+
+            crane.setTargetPosition(CRANE_LOW);
             crane.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
         }
 
         public void loop() {
             crane.setPower(1.0);
 
-            switch (CraneState) {
-                case LiftState.LIFT_START:
-                    // Waiting for some input
-                    if (gamepad1.x) {
+            switch (CS) {
+                case CRANE_START:
+                    if (gamepad2.x) {
                         // x is pressed, start extending
-                        liftMotor.setTargetPosition(LIFT_HIGH);
-                        liftState = LiftState.LIFT_EXTEND;
+                        crane.setTargetPosition(CRANE_HIGH);
+                        CS = CraneState.CRANE_UP;
+                    }
+                    if (gamepad2.y) {
+                        // x is pressed, start extending
+                        crane.setTargetPosition(CRANE_MID);
+                        CS = CraneState.CRANE_UP;
+                    }
+                    if (gamepad2.b) {
+                        // x is pressed, start extending
+                        crane.setTargetPosition(CRANE_LOW);
+                        CS = CraneState.CRANE_UP;
                     }
                     break;
-                case LiftState.LIFT_EXTEND:
-                    // check if the lift has finished extending,
-                    // otherwise do nothing.
-                    if (Math.abs(liftMotor.getCurrentPosition() - LIFT_HIGH) < 10) {
-                        // our threshold is within
-                        // 10 encoder ticks of our target.
-                        // this is pretty arbitrary, and would have to be
-                        // tweaked for each robot.
+                case CRANE_UP:
+                    if (Math.abs(crane.getCurrentPosition() - CRANE_HIGH) < 10) {
 
-                        // set the lift dump to dump
-                        liftDump.setTargetPosition(DUMP_DEPOSIT);
+                        /*liftDump.setTargetPosition(DUMP_DEPOSIT);
 
                         liftTimer.reset();
-                        liftState = LiftState.LIFT_DUMP;
+                        liftState = LiftState.LIFT_DUMP;*/
+
                     }
                     break;
-                case LiftState.LIFT_DUMP:
+                case CRANE_DUMP:
                     if (liftTimer.seconds() >= DUMP_TIME) {
                         // The robot waited long enough, time to start
                         // retracting the lift
-                        liftDump.setTargetPosition(DUMP_IDLE);
+                        /*liftDump.setTargetPosition(DUMP_IDLE);
                         liftMotor.setTargetPosition(LIFT_LOW);
-                        liftState = LiftState.LIFT_RETRACT;
+                        liftState = LiftState.LIFT_RETRACT;*/
                     }
-                    break;
-                case LiftState.LIFT_RETRACT:
-                    if (Math.abs(liftMotor.getCurrentPosition() - LIFT_LOW) < 10) {
-                        liftState = LiftState.LIFT_START;
+                case CRANE_DOWN:
+                    if (Math.abs(crane.getCurrentPosition() - CRANE_LOW) < 10) {
+                        CS = CraneState.CRANE_START;
                     }
                     break;
                 default:
                     // should never be reached, as liftState should never be null
-                    liftState = LiftState.LIFT_START;
+                    CS = CraneState.CRANE_START;
             }
-
-            // small optimization, instead of repeating ourselves in each
-            // lift state case besides LIFT_START for the cancel action,
-            // it's just handled here
-            if (gamepad1.y && liftState != LiftState.LIFT_START) {
-                liftState = LiftState.LIFT_START;
+            if (gamepad2.y && CS != CraneState.CRANE_START) {
+                CS = CraneState.CRANE_START;
             }
-
-            // mecanum drive code goes here
-            // But since none of the stuff in the switch case stops
-            // the robot, this will always run!
-            updateDrive(gamepad1, gamepad2);
         }
     }
 }
