aaba23ad8e065adfd515a6f1d2e88c8ea1495cfc
KingTryHard1 <codeshb7@gmail.com>
2023-04-22 16:33:59 -0400

    FSM 1.1 now working with drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingArea.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingArea.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 
@@ -14,7 +14,17 @@ public class TestingArea extends OpMode {
     public DcMotor crane;
     public CRServo lefts;
 
+    public DcMotorSimple frontLeft;
+    public DcMotorSimple frontRight;
+    public DcMotorSimple backLeft;
+    public DcMotorSimple backRight;
+
     double cranepower;
+
+    double turn;
+    double throttle;
+    float strafeLeft;
+    float strafeRight;
 
     public enum CraneState{
         CRANE_START,
@@ -29,17 +39,49 @@ public class TestingArea extends OpMode {
 
     @Override
     public void init() {
-        cranepower = gamepad2.right_stick_y;
 
         crane = hardwareMap.get(DcMotorEx.class, "CraneFront");
         lefts = hardwareMap.get(CRServo.class, "LeftsFront");
+
+        frontLeft = hardwareMap.get(DcMotorSimple.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorSimple.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotorSimple.class, "backLeft");
+        backRight = hardwareMap.get(DcMotorSimple.class, "backRight");
 
         crane.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         crane.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
+
     @Override
     public void loop() {
+
+        cranepower = gamepad2.right_stick_y;
+        throttle = gamepad1.left_stick_y;
+        turn = gamepad1.right_stick_x;
+        strafeLeft = gamepad1.left_trigger;
+        strafeRight = gamepad1.right_trigger;
+
+        frontLeft.setPower(strafeLeft);
+        frontRight.setPower(-strafeLeft);
+        backLeft.setPower(-strafeLeft);
+        backRight.setPower(strafeLeft);
+
+        frontLeft.setPower(-strafeRight);
+        frontRight.setPower(strafeRight);
+        backLeft.setPower(strafeRight);
+        backRight.setPower(-strafeRight);
+
+        frontLeft.setPower(throttle);
+        frontRight.setPower(throttle);
+        backLeft.setPower(throttle);
+        backRight.setPower(throttle);
+
+        frontLeft.setPower(-turn);
+        frontRight.setPower(turn);
+        backLeft.setPower(-turn);
+        backRight.setPower(turn);
+
         switch (CS) {
             case CRANE_START:
                 if (gamepad2.cross) {
@@ -66,8 +108,7 @@ public class TestingArea extends OpMode {
                         CS = CraneState.CRANE_DUMP;
                     }
                 }
-
-
+                break;
             case CRANE_DUMP:
                 if (gamepad2.square) {
                     crane.setTargetPosition(0);
@@ -77,6 +118,9 @@ public class TestingArea extends OpMode {
                         CS = CraneState.CRANE_START;
                     }
                 }
+                break;
+            default:
+                CS = CraneState.CRANE_START;
 
         }
     }
